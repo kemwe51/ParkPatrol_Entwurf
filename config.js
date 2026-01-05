23514da40ec98060d8d3c31f63626f11860f0975
kemwe51 <david.pfister@kemwe.ch>
2026-01-05 23:57:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // IMPORTANT: Put your Supabase anon public key below (safe for frontend).
 export const APP_NAME = "ParkPatrol";
 export const SUPABASE_URL = "https://xozrfllhpwpnrjovowsm.supabase.co";
-export const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_PUBLIC_KEY_HERE";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvenJmbGxocHdwbnJqb3Zvd3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NDI3NjQsImV4cCI6MjA4MzIxODc2NH0.IQvZThvQ-UBNsl5LnyHZ_CvKZM3Y11tnwj1-JT-Gc6Q";
